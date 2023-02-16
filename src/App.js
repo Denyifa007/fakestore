c8848ba4a1ed0789f900061fc19ac63c59428cfe
denyifa007 <denyifasamuel@hotmail.com>
2023-02-16 14:33:33 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+// import Fetcher from './componenets/Fetcher';
+import Home from './componenets/Home';
+import About from './componenets/About';
+import Products from './componenets/Products';
+import Error from './componenets/Error';
+import { BrowserRouter, Routes,Route } from 'react-router-dom';
+import Footer from './componenets/Footer';
+import Navbar from './componenets/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+      <Navbar/>
+      <Routes>
+        <Route index element ={<Home/>}/>
+        <Route path="About" element ={<About/>}/>
+        <Route path="Products" element ={<Products/>}/>
+        <Route path="*" element ={<Error/>}/>
+        
+      </Routes>
+      <Footer/>
+      </BrowserRouter>
+      {/* <Fetcher/> */}
+
+ 
     </div>
   );
 }
